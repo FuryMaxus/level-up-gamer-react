51a3fc92055f36d31366446a0caba055ab3f2145
John Doe <vicenteeast95@gmail.com>
2025-10-17 13:52:34 -0300

    Creación de Registro de cuenta e inicio de sesion (No terminado)

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import '../../styles/Form.css'
+import { useRef } from 'react'
+import { useState } from 'react'
 
-export default function Login() {
+
+function Login() {
+    const [error, setError] = useState('')
+    const [email, setEmail] = useState('')
+    const formRef = useRef(null)
+    
+  
+    
+    const handleEmail = (e) => {
+      const valor = e.target.value;
+      setEmail(valor);
+      setError(!valor.includes('@') ? 'Error, el email debe incluir (@)': ''); 
+    }
+    const handleSubmit = (e) => {
+      const inputs = formRef.current.querySelectorAll('input');
+      for(let input of inputs){
+        if(input.classList.contains('error')){
+          e.preventDefault();
+          setError('Error, revise los campos en rojo.')
+          return;
+        }
+      }
+    }
+      
+  
   return (
-    <>
+    <form action="#" id="formulario" ref={formRef} onSubmit={handleSubmit}>
+        <h1>Inicia Sesión en tu cuenta LEVEL UP GAMER</h1>
 
-    </>
+        <div className="row">
+            <label htmlfor="email">E-mail</label>
+            <input type="text" id="email" placeholder="Ingresa tu E-mail"
+            onChange={handleEmail} value={email}
+            className={!email.includes('@')? 'error' : ''}
+            />
+        </div>
+
+        <div className="row">
+            <label htmlfor="clave1">Contraseña</label>
+            <input type="password" id="clave1" placeholder="Ingresa tu contraseña"/>
+        </div>
+
+        <p id="errores">{error}</p>
+        <button type="sumbit">Iniciar Sesión</button>
+        <button type="reset">Limpiar</button>
+
+        <p>¿Aún no tienes una cuenta? <a href="#">Regístrate</a></p>
+    </form>
   )
 }
+
+export default Login
--- src/components/pages/SignOn.jsx
+++ src/components/pages/SignOn.jsx
@@ -1,9 +1,82 @@
 import React from 'react'
+import '../../styles/Form.css'
+import { useRef } from 'react'
+import { useState } from 'react'
 
-export default function SignOn() {
+
+function SignOn() {
+    const [nombre, setNombre] = useState('')
+    const [error, setError] = useState('')
+    const [email, setEmail] = useState('')
+    const formRef = useRef(null)
+    
+  
+    const handleName = (e) => {
+      const valor = e.target.value;
+      setNombre(valor);
+      setError(valor.length < 3 ? 'Error, ingrese mínimo 3 caracteres.' : ''); 
+    }
+    const handleEmail = (e) => {
+      const valor = e.target.value;
+      setEmail(valor);
+      setError(!valor.includes('@') ? 'Error, el email debe incluir (@)': ''); 
+    }
+    const handleSubmit = (e) => {
+      const inputs = formRef.current.querySelectorAll('input');
+      for(let input of inputs){
+        if(input.classList.contains('error')){
+          e.preventDefault();
+          setError('Error, revise los campos en rojo.')
+          return;
+        }
+      }
+    }
+      
+  
   return (
-    <>
+    <form action="#" id="formulario" ref={formRef} onSubmit={handleSubmit}>
+        <h1>Crear tu cuenta LEVEL UP GAMER</h1>
+        
+        
+        <div className="row">
+            <label htmlfor="nombre">Nombre Completo</label>
+            <input type="text" id="nombres" placeholder="Joe Bryan Black Man"
+            onChange={handleName} value={nombre}
+            className={nombre.length <3 ? 'error' : ''}
+            />
+        </div>
 
-    </>
+        <div className="row">
+            <label htmlfor="email">E-mail</label>
+            <input type="text" id="email" placeholder="joeblack777@gmail.com"
+            onChange={handleEmail} value={email}
+            className={!email.includes('@')? 'error' : ''}
+            />
+        </div>
+
+        <div className="row">
+            <label htmlfor="direccion">Dirección</label>
+            <input type="text" id="direccion" placeholder="Pardo #777"/>
+        </div>
+
+        <div className="row">
+            <label htmlfor="contraseña1">Contraseña</label>
+            <input type="password" id="clave1" placeholder="Clave123456"/>
+        </div>
+
+        <div className="row">
+            <label htmlfor="contraseña2">Confirmar contraseña</label>
+            <input type="password" id="clave2" placeholder="Clave123456"/>
+        </div>
+
+        <p id="errores">{error}</p>
+  
+        <button type="sumbit">Registrarse</button>
+        <button type="reset">Limpiar</button>
+
+        <p>¿Ya tienes una cuenta? <a href="#">Inicia Sesión</a></p>
+    </form>
   )
 }
+
+export default SignOn
